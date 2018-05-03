a11d4a8f7c637735eff42ab137a076bfbf3a70fa
Matthew Ishige <matthewlujp@gmail.com>
2018-05-03 11:59:15 +0900

    Implement Related video search

--- src/youtube_data_v3/video_client.go
+++ src/youtube_data_v3/video_client.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	searchURL        = "https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=%s&maxResults=%d&key=%s"                // provide keywords(joined using %2C), max results, api key
+	searchURL        = "https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=%s&maxResults=%d&key=%s"                // provide keywords(joined using ","), max results, api key
 	relatedSearchURL = "https://www.googleapis.com/youtube/v3/search?part=id&relatedToVideoId=%s&type=video&maxResults=%d&key=%s" // provide videoID,  max results, api key
 	videoInfoURL     = "https://www.googleapis.com/youtube/v3/videos?part=id,snippet,contentDetails,statistics&id=%s&key=%s"      // provide videoID, api key
 )
@@ -49,8 +49,8 @@ func init() {
 // Search returns detailed info of videos hit for given keywords.
 // It takes a []string for keywords and maximum number of videos for the search result.
 // This method returns a []Video and error if any.
-// Under the hood, the method issues GET request to YouTube Data v3 API twice,
-//   1. to retrieve video ids related to given keywords and
+// In the method, GET request to YouTube Data v3 API is issued twice,
+//   1. to retrieve video ids related to given keywords
 //   2. to retrieve detailed video info for ids obtained via the previous step
 func (c *impleVideoClient) Search(keywords []string, maxResults int) ([]Video, error) {
 	// search with keyword and obtain video ids
@@ -79,12 +79,12 @@ func (c *impleVideoClient) Search(keywords []string, maxResults int) ([]Video, e
 	reqURL = fmt.Sprintf(videoInfoURL, strings.Join(ids, ","), c.apiKey)
 	req, errBuildReq = http.NewRequest("GET", reqURL, nil)
 	if errBuildReq != nil {
-		return nil, fmt.Errorf("failed in detailed info for searched videos, %s", errBuildReq)
+		return nil, fmt.Errorf("failed in retrieving detailed info for searched videos, %s", errBuildReq)
 	}
 	// GET request to YouTube Data v3 API for detailed info of each video id
 	res, errReq = c.client.Do(req)
 	if errReq != nil {
-		return nil, fmt.Errorf("failed in detailed info for searched videos, %s", errReq)
+		return nil, fmt.Errorf("failed in retrieving detailed info for searched videos, %s", errReq)
 	}
 	defer res.Body.Close()
 	if res.StatusCode != http.StatusOK {
@@ -95,8 +95,54 @@ func (c *impleVideoClient) Search(keywords []string, maxResults int) ([]Video, e
 	return parseVideosDetails(res.Body)
 }
 
-func (c *impleVideoClient) Related(id string, maxResults int) ([]Video, error) {
-	return nil, errors.New("not implemented")
+// Related returns detailed info of videos which is related to a given video id.
+// It takes a video id and maximux number videos in a result.
+// The method returns a []Video and error if any.
+// In the method, GET request to YouTube Data v3 API is issued twice,
+//   1. to retrieve video ids related to given video id (same as search reqest)
+//   2. to retrieve detailed video info for ids obtained via the previous step
+func (c *impleVideoClient) Related(searchID string, maxResults int) ([]Video, error) {
+	// search with searchID and obtain ids of related videos
+	reqURL := fmt.Sprintf(relatedSearchURL, searchID, maxResults, c.apiKey)
+	req, errBuildReq := http.NewRequest("GET", reqURL, nil)
+	if errBuildReq != nil {
+		return nil, fmt.Errorf("failed in searching related videos, %s", errBuildReq)
+	}
+	// GET request to YouTube Data v3 API for searching video ids
+	res, errReq := c.client.Do(req)
+	if errReq != nil {
+		return nil, fmt.Errorf("failed in searching related videos, %s", errReq)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("response for %s got status %s", reqURL, res.Status)
+	}
+
+	// extract ids from json response
+	ids, errIDs := parseSearchResult(res.Body)
+	if errIDs != nil {
+		return nil, fmt.Errorf("failed in searching related videos, %s", errIDs)
+	}
+
+	// retrieve detailed video info for obtained video ids
+	reqURL = fmt.Sprintf(videoInfoURL, strings.Join(ids, ","), c.apiKey)
+	req, errBuildReq = http.NewRequest("GET", reqURL, nil)
+	if errBuildReq != nil {
+		return nil, fmt.Errorf("failed in retrieving detailed info for related videos, %s", errBuildReq)
+	}
+	// GET request to YouTube Data v3 API for detailed info of each video id
+	res, errReq = c.client.Do(req)
+	if errReq != nil {
+		return nil, fmt.Errorf("failed in retrieving detailed info for related videos, %s", errReq)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("response for %s got status %s", reqURL, res.Status)
+	}
+
+	// extract necessary data from returned json
+	return parseVideosDetails(res.Body)
+
 }
 
 func (c *impleVideoClient) Get(id string) (*Video, error) {
--- src/youtube_data_v3/video_client_test.go
+++ src/youtube_data_v3/video_client_test.go
@@ -13,8 +13,10 @@ import (
 )
 
 const (
-	searchResultJSONPath  = "./mocks/data/search_result.json"
-	searchDetailsJSONPath = "./mocks/data/search_details.json"
+	searchResultJSONPath   = "./mocks/data/search_result.json"
+	searchDetailsJSONPath  = "./mocks/data/search_details.json"
+	relatedResultJSONPath  = "./mocks/data/related_result.json"
+	relatedDetailsJSONPath = "./mocks/data/related_details.json"
 )
 
 func TestSearch(t *testing.T) {
@@ -23,7 +25,7 @@ func TestSearch(t *testing.T) {
 	c := mock.NewMockClient(ctrl)
 
 	apiKey := "foobar"
-	req, errReq := http.NewRequest("GET", "https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=violet&maxResults=3&key="+apiKey, nil)
+	req, errReq := http.NewRequest("GET", fmt.Sprintf(searchURL, "violet", 3, apiKey), nil)
 	if errReq != nil {
 		t.Fatal(errReq)
 	}
@@ -41,7 +43,7 @@ func TestSearch(t *testing.T) {
 	if videos, err := DefaultVideoClient.Search([]string{"violet"}, 3); err != nil {
 		t.Errorf("search failed, %s", err)
 	} else {
-		// check only number a of obtained videos and test one video info details
+		// check only number of obtained videos and test one video info details
 		if len(videos) != 3 {
 			t.Errorf("number of videos expected %d, got %d", 3, len(videos))
 		}
@@ -66,6 +68,64 @@ func TestSearch(t *testing.T) {
 					High:     ThumbnailDetail{URL: "https://i.ytimg.com/vi/UZxz9ot7y0Y/hqdefault.jpg", Width: 480, Height: 360},
 					Standard: ThumbnailDetail{URL: "https://i.ytimg.com/vi/UZxz9ot7y0Y/sddefault.jpg", Width: 640, Height: 480},
 					Maxres:   ThumbnailDetail{URL: "https://i.ytimg.com/vi/UZxz9ot7y0Y/maxresdefault.jpg", Width: 1280, Height: 720},
+				}
+				if !reflect.DeepEqual(v.Thumbnails, expectedThumbnails) {
+					t.Errorf("id=%s: thumbnails expected %v, got %v ", v.ID, expectedThumbnails, v.Thumbnails)
+				}
+			}
+		}
+	}
+
+}
+func TestRelated(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	c := mock.NewMockClient(ctrl)
+
+	apiKey := "foobar"
+	req, errReq := http.NewRequest("GET", fmt.Sprintf(relatedSearchURL, "RiCql90xh7Q", 3, apiKey), nil)
+	if errReq != nil {
+		t.Fatal(errReq)
+	}
+	// request for related video id search
+	c.EXPECT().Do(req).Return(returnFileAsResponse(relatedResultJSONPath))
+
+	// request for relted video details
+	req, errReq = http.NewRequest("GET", fmt.Sprintf(videoInfoURL, "lhu8HWc9TlA,mc7GUZinTD0,6qptaGpilE0", apiKey), nil)
+	c.EXPECT().Do(req).Return(returnFileAsResponse(relatedDetailsJSONPath))
+
+	DefaultVideoClient.apiKey = apiKey
+	DefaultVideoClient.client = c
+
+	// check related result
+	if videos, err := DefaultVideoClient.Related("RiCql90xh7Q", 3); err != nil {
+		t.Errorf("related failed, %s", err)
+	} else {
+		// check only number of obtained videos and test one video info details
+		if len(videos) != 3 {
+			t.Errorf("number of videos expected %d, got %d", 3, len(videos))
+		}
+		for _, v := range videos {
+			// check video info details for id = lhu8HWc9TlA
+			if v.ID == "lhu8HWc9TlA" {
+				if v.Title != "Violet Evergarden - COMPLETE ALBUM - [ ヴァイオレット・エヴァーガーデン ] [BGM]" {
+					t.Errorf("id=%s: title expected %s, got %s ", v.ID, "Violet Evergarden - COMPLETE ALBUM - [ ヴァイオレット・エヴァーガーデン ] [BGM]", v.Title)
+				}
+				if v.Duration != time.Hour*1+time.Minute*46+time.Second*52 {
+					t.Errorf("id=%s: duration expected %s, got %s ", v.ID, time.Hour*1+time.Minute*46+time.Second*52, v.Duration)
+				}
+				if v.ViewCount != 9195 {
+					t.Errorf("id=%s: view count expected %d, got %d ", v.ID, 9195, v.ViewCount)
+				}
+				if v.PublishDate != "2018-03-28" {
+					t.Errorf("id=%s: publish date expected %s, got %s ", v.ID, "2018-03-28", v.PublishDate)
+				}
+				expectedThumbnails := Thumbnails{
+					Default:  ThumbnailDetail{URL: "https://i.ytimg.com/vi/lhu8HWc9TlA/default.jpg", Width: 120, Height: 90},
+					Medium:   ThumbnailDetail{URL: "https://i.ytimg.com/vi/lhu8HWc9TlA/mqdefault.jpg", Width: 320, Height: 180},
+					High:     ThumbnailDetail{URL: "https://i.ytimg.com/vi/lhu8HWc9TlA/hqdefault.jpg", Width: 480, Height: 360},
+					Standard: ThumbnailDetail{URL: "https://i.ytimg.com/vi/lhu8HWc9TlA/sddefault.jpg", Width: 640, Height: 480},
+					Maxres:   ThumbnailDetail{},
 				}
 				if !reflect.DeepEqual(v.Thumbnails, expectedThumbnails) {
 					t.Errorf("id=%s: thumbnails expected %v, got %v ", v.ID, expectedThumbnails, v.Thumbnails)
